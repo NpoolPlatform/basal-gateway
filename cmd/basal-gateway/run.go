c9f9b0b99ea8f60a31bbe91fdc125d8b6c9f3481
Zhao KK <kikakkz@hotmail.com>
2023-09-14 03:20:44 +0000

    Update commit

--- cmd/basal-gateway/run.go
+++ cmd/basal-gateway/run.go
@@ -1,35 +1,50 @@
 package main
 
 import (
+	"context"
+
 	"github.com/NpoolPlatform/basal-gateway/api"
-
-	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-
-	cli "github.com/urfave/cli/v2"
-
 	apicli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
-
+	"github.com/NpoolPlatform/go-service-framework/pkg/action"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	cli "github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
 )
-
-// const MsgInterval = 3 * time.Second
 
 var runCmd = &cli.Command{
 	Name:    "run",
 	Aliases: []string{"s"},
 	Usage:   "Run the daemon",
 	Action: func(c *cli.Context) error {
-		go func() {
-			if err := grpc2.RunGRPC(rpcRegister); err != nil {
-				logger.Sugar().Errorf("fail to run grpc server: %v", err)
-			}
-		}()
+		err := action.Run(
+			c.Context,
+			run,
+			rpcRegister,
+			rpcGatewayRegister,
+			watch,
+		)
 
-		return grpc2.RunGRPCGateWay(rpcGatewayRegister)
+		return err
 	},
+}
+
+func run(ctx context.Context) error {
+	return nil
+}
+
+func shutdown(ctx context.Context) {
+	<-ctx.Done()
+	logger.Sugar().Infow(
+		"Watch",
+		"State", "Done",
+		"Error", ctx.Err(),
+	)
+}
+
+func watch(ctx context.Context, cancel context.CancelFunc) error {
+	go shutdown(ctx)
+	return nil
 }
 
 func rpcRegister(server grpc.ServiceRegistrar) error {
@@ -45,8 +60,5 @@ func rpcGatewayRegister(mux *runtime.ServeMux, endpoint string, opts []grpc.Dial
 	if err != nil {
 		return err
 	}
-
-	_ = apicli.Register(mux)
-
 	return nil
 }
